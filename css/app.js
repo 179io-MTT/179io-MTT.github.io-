ced62fbebcdcf3bbaa60ee7445aca73c8e792777
iLos.HN <125381497+ilosipa@users.noreply.github.com>
2023-06-04 20:29:22 +0700

    Add files via upload
    Signed-off-by: iLos.HN <125381497+ilosipa@users.noreply.github.com>

---
+++ css/app.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById( popup’)
+
+function openPopup( ){
+popup.classList.add( 'open-popup')
+i;
+
+function closePopup(){
+popup.classList.remove( 'open-popup')
+
+b
+
