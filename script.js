6dfb9c5d0b1c337c9ed8ed0540b7408a0736d058
little179 <125381497+little179@users.noreply.github.com>
2024-08-18 15:59:44 +0700

    Add files via upload
    Signed-off-by: little179 <125381497+little179@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,159 +1,78 @@
-'use strict';
-
-
-
-// element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
-// sidebar variables
-const sidebar = document.querySelector("[data-sidebar]");
-const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-
-// sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
-
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-// modal variable
-const modalImg = document.querySelector("[data-modal-img]");
-const modalTitle = document.querySelector("[data-modal-title]");
-const modalText = document.querySelector("[data-modal-text]");
-
-// modal toggle function
-const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
-
-// add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
-
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-    testimonialsModalFunc();
-
-  });
-
-}
-
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
-
-
-
-// custom select variables
-const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
-select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
-  });
-}
-
-// filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
-
-const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
-
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
-
-for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
-}
-
-
-
-// contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
-
-
-
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
-
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+document.addEventListener('DOMContentLoaded', () => {
+    const usernameInput = document.getElementById('username');
+    const messageInput = document.getElementById('message');
+    const totalSent = document.getElementById('total-sent');
+    const totalReq = document.getElementById('total-req');
+    const sendButton = document.getElementById('send-button');
+    const responseText = document.getElementById('response');
+    let spamInterval;
+  
+    let sentTotal = totalSent.innerHTML;
+    let total_sent = parseInt(sentTotal);
+  
+    let reqTotal = totalReq.innerHTML;
+    let req_sent = parseInt(reqTotal);
+  
+    sendButton.addEventListener('click', async () => {
+      const usernameValue = usernameInput.value;
+      const messageValue = messageInput.value;
+  
+      if (!usernameValue) {
+        alert('Vui Long Điền UserName ở Link');
+        return;
       }
+  
+      if (!messageValue) {
+        alert('Vui Lòng Điền Messenger Muốn Gửip');
+        return;
+      }
+  
+      sendButton.disabled = true;
+      sendButton.textContent = 'Đang Tiến Hành Spam';
+      usernameInput.setAttribute('readonly', true);
+      messageInput.setAttribute('readonly', true);
+  
+      if (!spamInterval) {
+        spamInterval = setInterval(async function spamFunction() {
+          try {
+            const data = await callSpam();
+            totalSent.textContent = (data.status == "Sent Success" ? ++total_sent : total_sent);
+            responseText.textContent = data.status;
+          } catch (err) {
+            const data = await callSpam();
+          } 
+        }, 500); 
+      }
+    });
+  
+    async function callSpam() {
+      req_sent++;
+      totalReq.textContent = req_sent;
+  
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: usernameInput.value,
+          message: messageInput.value
+        })
+      };
+  
+      const response = await fetch('/api/send', requestOptions);
+  
+      if (!response.ok) {
+        if (response.status === 500) {
+          throw new Error('Server error. Please try again later.');
+        }
+        const data = await response.json();
+        throw new Error(data.error || 'Something went wrong');
+      }
+  
+      const data = await response.json();
+      return data; 
     }
-
   });
-}
+  
+  
